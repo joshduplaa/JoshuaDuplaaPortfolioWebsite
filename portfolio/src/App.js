f26e7b1c58e0c00447a3c2a32e639daa6a232197
Joshua Duplaa <joshuaduplaa@outlook.com>
2023-12-27 02:43:14 -0600

    started and deployed portfolio website

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+// App.js
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <h2>Portfolio Website under construction</h2>
+        <p>Github: https://github.com/joshduplaa</p>
+        <p>Check out projects like the SWE Lottery Project or AVR Assembly Asteroids Game</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
