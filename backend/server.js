72f2202f7dcfb2d22562d233cf38d34ae01c6964
Joshua Duplaa <joshuaduplaa@outlook.com>
2025-10-11 21:38:40 -0500

    set up GCP and backend K8s deployment structure

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import { exec } from "child_process";
 
 const app = express();
 
-app.post("/launch-env", (_, res) => {
+app.post("/launch-env", (req, res) => {
   exec("bash ./scripts/launch_k8s_env.sh", (err, stdout, stderr) => {
     if (err) return res.status(500).send(stderr);
     res.send(stdout);
